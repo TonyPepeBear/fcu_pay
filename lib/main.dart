d7cdb2449e03c2ba7456b57d10bfc3fe3c5275da
TonyPepe <tonybear55665566@gmail.com>
2022-07-20 22:28:57 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      backgroundColor: Color(int.parse("822731", radix: 16) | 0xFF000000),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset("assets/images/FCU_LOGO.png"),
+            const SizedBox(height: 30),
             const Text(
-              'You have pushed the button this many times:',
+              "FCU  Pay",
+              style: TextStyle(color: Colors.white, fontSize: 25),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            const SizedBox(height: 80),
+            OutlinedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                child: const Text("校方", style: TextStyle(fontSize: 25))),
+            const SizedBox(height: 35),
+            OutlinedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                child: const Text("學生/老師", style: TextStyle(fontSize: 25))),
+            const SizedBox(height: 35),
+            OutlinedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                child: const Text(
+                  "合作商家",
+                  style: TextStyle(fontSize: 25),
+                ))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
